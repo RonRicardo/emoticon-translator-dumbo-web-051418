5fbe189853f34d2ff49f31381018e367ce08f062
Roniece Ricardo <roniecericardo@gmail.com>
2018-05-14 02:41:06 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,13 @@ require 'yaml'
 Error_message = "Sorry, that emoticon was not found"
 
 def load_library(file_path)
- return lib = YAML.load_file(file_path)
+ lib = YAML.load_file(file_path)
+ emoticon_lib = {"get_meaning" => {}, "get_emoticion" => {} }
+ lib.each do |meaning, emoticons|
+   emoticon_lib["get_meaning"][emoticons[1]] = meaning
+   emoticon_lib["get_emoticion"][emoticion[0]] = emoticion
+ end
+ emoticon_lib
 end
 
 def get_japanese_emoticon
